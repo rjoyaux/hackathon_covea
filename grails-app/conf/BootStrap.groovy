80609cb852557cf0b2b4fb40c3185c054150cac2
lio <lio@V3450>
2015-06-04 17:21:01 +0200

    ok

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,15 +1,15 @@
 import car.advisor.Vehicule
+import car.advisor.Utilisateur
 
 class BootStrap {
 
     def init = { servletContext ->
 		
 		//chargement du fichier csv
-		log.info('chargement des vehicules')
+		log.error('chargement des vehicules')
 		def csv = new File("grails-app/conf/versions_voitures.csv")
 		def vehicule
-		csv.splitEachLine(',') { row ->
-			log.info(row[0])
+		csv.splitEachLine(',') { row ->			
 			vehicule = new Vehicule (
 			   identifiant : row[0],
 			   marque: row[1],
@@ -19,12 +19,26 @@ class BootStrap {
 			   declinaison:row[5],
 			   urlImage:row[32]
 			   ) 
-			log.info(vehicule)
 			vehicule.save(failOnError: true, flush: true)
-			
 		}
-			
-			
+		
+		log.error('chargement des utilisateurs')
+		csv = new File("grails-app/conf/fichier_client.csv")
+		def utilisateur
+		csv.splitEachLine(';') { row ->
+			utilisateur = new Utilisateur (
+				nom : row[0],
+				prenom : row[1],
+				ville : row[2],
+				age : row[3].toInteger(),
+				nbEnfants : row[4].toInteger(),
+				refPers : row[5]
+				// identifiant vehicule
+				// date d'assurance
+			)
+			utilisateur.save(failOnError: true, flush: true)
+		}
+		
     }
     def destroy = {
     }
